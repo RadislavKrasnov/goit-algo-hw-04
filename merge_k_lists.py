3a7288226368112dd4f857266f9db4f460f7102a
Radyslav Krasnov <radislavkrasnoff@gmail.com>
2024-07-15 09:53:02 +0300

    add merge_k_lists task

---
+++ merge_k_lists.py
@@ -0,0 +1,42 @@
+from functools import reduce
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    return merge(merge_sort(left_half), merge_sort(right_half))
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    while left_index < len(left):
+        merged.append(left[left_index])
+        left_index += 1
+
+    while right_index < len(right):
+        merged.append(right[right_index])
+        right_index += 1
+
+    return merged
+
+def merge_k_lists(lists):
+    reduced_list = reduce(lambda x, y: x + y , lists)
+    return merge_sort(reduced_list)
+
+lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
+merged_list = merge_k_lists(lists)
+print("Відсортований список:", merged_list)
